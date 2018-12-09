8e30b62010f9705cddff7f78595ba722d93c2356
mindblastr <rafael.rpereira@hotmail.com>
2018-12-09 23:55:58 +0000

    Added fileread function. Did not add to main.py since i'm not sure where the fuck it's suppose to go, or how.

---
+++ School_Scheduling/fileread.py
@@ -0,0 +1,19 @@
+
+with open('input_1.txt') as fh:
+    inputInfo = dict()
+    for line in fh.readlines():
+        line = line.strip()
+        split_line = line.split(' ')
+        auxList = list()
+        
+        for value in split_line[1:]:
+            auxList.append(value)
+        
+        newTuple=tuple(auxList)
+        inputInfo.update({split_line[0]:newTuple})
+
+    print(inputInfo)        
+
+
+
+
